d276977449240cb80726b6d08f822fd0f19f80fe
Selman Sahin <selmansahin@gmail.com>
2025-04-09 19:02:41 +0300

    Update BIST stock symbols list from selected stocks to full BIST 100 index

--- bist_momentum_analyzer.py
+++ bist_momentum_analyzer.py
@@ -7,20 +7,33 @@ warnings.filterwarnings('ignore')
 
 def get_bist_symbols():
     """
-    Türkiye borsasındaki (BIST) hisse senetlerinin sembollerini döndürür.
+    Türkiye borsasındaki (BIST 100) hisse senetlerinin sembollerini döndürür.
+    BIST 100, Borsa İstanbul'da işlem gören en yüksek piyasa değerine sahip 100 hissenin yer aldığı endekstir.
     """
-    # BIST'teki bazı büyük şirketlerin sembolleri
+    # BIST 100 hisse sembolleri (güncel)
     # BIST hisselerinin Yahoo Finance'deki formatı: SEMBOL.IS
     symbols = [
-        'AKBNK.IS', 'ARCLK.IS', 'ASELS.IS', 'BIMAS.IS', 'EKGYO.IS', 
-        'EREGL.IS', 'FROTO.IS', 'GARAN.IS', 'HALKB.IS', 'ISCTR.IS', 
-        'KCHOL.IS', 'KOZAA.IS', 'KOZAL.IS', 'KRDMD.IS', 'MGROS.IS', 
-        'PGSUS.IS', 'SAHOL.IS', 'SISE.IS', 'TAVHL.IS', 'TCELL.IS', 
-        'THYAO.IS', 'TOASO.IS', 'TUPRS.IS', 'VAKBN.IS', 'YKBNK.IS',
-        'ALARK.IS', 'DOHOL.IS', 'ENKAI.IS', 'OTKAR.IS', 'PETKM.IS',
-        'SASA.IS', 'HEKTS.IS', 'OYAKC.IS', 'SOKM.IS' ,'TTKOM.IS',
-        'ASTOR.IS', 'ULKER.IS', 'TKFEN.IS', 'ODAS.IS', 'ENJSA.IS',
-        'AKSEN.IS', 'CIMSA.IS', 'GUBRF.IS'
+        'AKBNK.IS', 'AKFYE.IS', 'AKGRT.IS', 'AKSA.IS', 'AKSEN.IS', 
+        'ALARK.IS', 'ALBRK.IS', 'ALFAS.IS', 'ALGYO.IS', 'ALKIM.IS', 
+        'ARCLK.IS', 'ASELS.IS', 'ASUZU.IS', 'AYGAZ.IS', 'BAGFS.IS', 
+        'BERA.IS', 'BIENY.IS', 'BIMAS.IS', 'BRISA.IS', 'BRSAN.IS', 
+        'BRYAT.IS', 'BUCIM.IS', 'CANTE.IS', 'CCOLA.IS', 'CIMSA.IS', 
+        'DEVA.IS', 'DOAS.IS', 'DOHOL.IS', 'ECILC.IS', 'EGEEN.IS', 
+        'EKGYO.IS', 'ENKAI.IS', 'EREGL.IS', 'EUPWR.IS', 'FROTO.IS',
+        'GARAN.IS', 'GENIL.IS', 'GESAN.IS', 'GLYHO.IS', 'GSDHO.IS',
+        'GUBRF.IS', 'GWIND.IS', 'HALKB.IS', 'HEKTS.IS', 'HLGYO.IS',
+        'IHLGM.IS', 'ISDMR.IS', 'ISFIN.IS', 'ISGYO.IS', 'ISMEN.IS',
+        'ISCTR.IS', 'ISMEN.IS', 'ITTFH.IS', 'KARSN.IS', 'KARTN.IS',
+        'KCHOL.IS', 'KLMSN.IS', 'KMPUR.IS', 'KONTR.IS', 'KORDS.IS',
+        'KOZAA.IS', 'KOZAL.IS', 'KRDMD.IS', 'LOGO.IS', 'MAVI.IS',
+        'MGROS.IS', 'NETAS.IS', 'NTHOL.IS', 'ODAS.IS', 'OTKAR.IS',
+        'OYAKC.IS', 'PENTA.IS', 'PETKM.IS', 'PGSUS.IS', 'QUAGR.IS',
+        'SAHOL.IS', 'SASA.IS', 'SELEC.IS', 'SISE.IS', 'SKBNK.IS',
+        'SMART.IS', 'SOKM.IS', 'TAVHL.IS', 'TCELL.IS', 'THYAO.IS',
+        'TKFEN.IS', 'TKNSA.IS', 'TLMAN.IS', 'TOASO.IS', 'TRGYO.IS',
+        'TSKB.IS', 'TTKOM.IS', 'TTRAK.IS', 'TUPRS.IS', 'ULKER.IS',
+        'VAKBN.IS', 'VERUS.IS', 'VESBE.IS', 'VESTL.IS', 'YKBNK.IS',
+        'YYLGD.IS', 'ZOREN.IS'
     ]
     return symbols
 
